ff30dc4f81eaf2a2bffd441e3c269ecfe18711ab
HyunSeongLee <devhyun05@gmail.com>
2023-12-11 02:29:27 -0500

    Revert "video modifyed"
    This reverts commit 54335c23b0ee5eddeafc3d983e48a9993525a4ec.

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'; 
 import { FaBars, FaCode, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'; 
 import { HiOutlineMail } from 'react-icons/hi'; 
+import {BsFillPersonLinesFill} from 'react-icons/bs'; 
 
 import { Link } from 'react-scroll'; 
 
